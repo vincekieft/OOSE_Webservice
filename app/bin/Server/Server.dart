f5e978278a83cb4a561114843c00609ecba5bd6a
LAPTOPVINCE\Vince Kieft <vincekieft1997@gmail.com>
2019-01-18 20:36:00 +0100

    Log requests

--- app/bin/Server/Server.dart
+++ app/bin/Server/Server.dart
@@ -19,6 +19,8 @@ class Server{
     print('Serving at http://${server.address.host}:${server.port}');
 
     await for (HttpRequest req in server) {
+      print("${req.method} Request: ${req.uri.toString()}");
+
       await _onRequest(req);
       req.response.close();
     }
